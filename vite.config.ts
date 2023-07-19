7c15913e93863ee95e7c29554b761b31e90a23e2
ZhongboHuang <huangzhongbo@cmict.chinamobile.com>
2023-07-19 17:05:47 +0800

    refactor: ♻️ 性能优化

--- vite.config.ts
+++ vite.config.ts
@@ -4,21 +4,58 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 import { visualizer } from 'rollup-plugin-visualizer'
+import { Plugin as importToCDN, autoComplete } from 'vite-plugin-cdn-import'
+import viteCompression from 'vite-plugin-compression'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
     vueJsx(),
+    // build 视图分析依赖文件
     visualizer({
       emitFile: false,
-      filename: 'dist/stats.html',
+      filename: 'stats.html',
+      gzipSize: true,
       open: true
+    }),
+    // 依赖包 CDN 引入
+    importToCDN({
+      modules: [
+        autoComplete('vue'),
+        {
+          name: 'vue-router',
+          var: 'VueRouter',
+          path: 'dist/vue-router.global.js'
+        },
+        {
+          name: 'pinia',
+          var: 'pinia',
+          path: 'dist/pinia.iife.min.js'
+        }
+      ]
+    }),
+    // gzip 压缩
+    viteCompression({
+      verbose: true,
+      algorithm: 'gzip',
+      ext: '.gz'
     })
   ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  },
+  build: {
+    outDir: 'dist',
+    rollupOptions: {
+      output: {
+        // 静态资源分类打包
+        chunkFileNames: 'assets/js/[name]-[hash].js',
+        entryFileNames: 'assets/js/[name]-[hash].js',
+        assetFileNames: 'assets/[ext]/[name]-[hash].[ext]'
+      }
     }
   }
 })
